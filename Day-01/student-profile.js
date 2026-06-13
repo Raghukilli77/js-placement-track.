69da4b29cfb4209b20319438efd1a142a11caa1d
Raghukilli77 <raghukilli99@gmail.com>
2026-06-13 11:01:37 +0530

    Day 1 - Student Profile Generator

---
+++ Day-01/student-profile.js
@@ -0,0 +1,13 @@
+let name = "Raghu";
+let age = 20;
+let branch = "CSE";
+let college = "BTech";
+
+console.log(`
+Student Profile
+---------------
+Name: ${name}
+Age: ${age}
+Branch: ${branch}
+College: ${college}
+`);
