3a6023ebccdd6c3033fe74af6d28f107f1fa57bd
Raghukilli77 <raghukilli99@gmail.com>
2026-06-20 16:47:20 +0530

    Create app.js

---
+++ Day-07/Text Changer/app.js
@@ -0,0 +1,6 @@
+// app.js
+let heading = document.querySelector("#title");
+let btn = document.querySelector("#btn");
+btn.addEventListener("click" , function(){
+    heading.innerText = "JS IS FUN";
+});
