c5acb7589ea5160e0ca531248387ea02c8ac8fe6
Raghukilli77 <raghukilli99@gmail.com>
2026-06-20 16:48:57 +0530

    Create app.js

---
+++ Day-07/Color Changer/app.js
@@ -0,0 +1,8 @@
+let btn1 = document.querySelector("#red");
+btn1.addEventListener("click", ()=>{
+    heading.style.color = "red";
+});
+let btn2 = document.querySelector("#blue");
+btn2.addEventListener("click", ()=>{
+    heading.style.color = "blue";
+})
